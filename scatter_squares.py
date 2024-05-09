bbfb9ff5edfdb30d9a11f7222b0412672a51dc5f
Tafiz <mostafezurrahman50@gmail.com>
2024-05-09 12:35:26 +0600

    plot scatter

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 x_values=list(range(1,1001))
 y_values=[x**2 for x in x_values]
-plt.scatter(x_values,y_values,edgecolor='none',s=40)
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,edgecolor='none',s=40)
+plt.savefig('squares_plot.png',bbox_inches='tight')
 
 #set chart title and label axes
 plt.title("Squares Numbers",fontsize=24)
